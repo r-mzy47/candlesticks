56097aae91baf7c987633e8f4e7bc65e84a60a79
reza <rmirzaee98@gmail.com>
2021-07-25 23:18:38 +0430

    time_row refactor / add doc

--- lib/src/widgets/time_row.dart
+++ lib/src/widgets/time_row.dart
@@ -15,14 +15,65 @@ class TimeRow extends StatelessWidget {
     required this.candleWidth,
     required this.scrollController,
   }) : super(key: key);
+
+  /// Calculates number of candles between two time indicator
+  int _stepCalculator() {
+    if (candleWidth < 3)
+      return 31;
+    else if (candleWidth < 5)
+      return 19;
+    else if (candleWidth < 7)
+      return 13;
+    else
+      return 9;
+  }
+
+  /// Calculates [DateTime] of a given candle index
+  DateTime _timeCalculator(int step, int index, Duration dif) {
+    int candleNumber = (step + 1) ~/ 2 - 10 + index * step + -1;
+    DateTime? _time;
+    if (candleNumber < 0)
+      _time = candles[step + candleNumber].date.add(dif);
+    else if (candleNumber < candles.length)
+      _time = candles[candleNumber].date;
+    else {
+      final stepsBack = (candleNumber - candles.length) ~/ step + 1;
+      final newIndex = candleNumber - stepsBack * step;
+      _time = candles[newIndex].date.subtract(dif * stepsBack);
+    }
+    return _time;
+  }
+
+  /// Fomats number as 2 digit integer
+  String numberFormat(int value) {
+    return "${value < 10 ? 0 : ""}$value";
+  }
+
+  /// Day/month text widget
+  Text _monthDayText(DateTime _time) {
+    return Text(
+      numberFormat(_time.day) + "/" + numberFormat(_time.month),
+      style: TextStyle(
+        color: ColorPalette.grayColor,
+        fontSize: 12,
+      ),
+    );
+  }
+
+  /// Hour/minute text widget
+  Text _hourMinuteText(DateTime _time) {
+    return Text(
+      numberFormat(_time.hour) + ":" + numberFormat(_time.minute),
+      style: TextStyle(
+        color: ColorPalette.grayColor,
+        fontSize: 12,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    int step = 9;
-    if (candleWidth < 3)
-      step = 31;
-    else if (candleWidth < 5)
-      step = 19;
-    else if (candleWidth < 7) step = 13;
+    int step = _stepCalculator();
     final dif = candles[0].date.difference(candles[step].date);
     return Padding(
       padding: const EdgeInsets.only(right: 51.0),
@@ -33,17 +84,7 @@ class TimeRow extends StatelessWidget {
         itemExtent: step * candleWidth,
         reverse: true,
         itemBuilder: (context, index) {
-          int candleNumber = (step + 1) ~/ 2 - 10 + index * step + -1;
-          DateTime? _time;
-          if (candleNumber < 0)
-            _time = candles[step + candleNumber].date.add(dif);
-          else if (candleNumber < candles.length)
-            _time = candles[candleNumber].date;
-          else {
-            final stepsBack = (candleNumber - candles.length) ~/ step + 1;
-            final newIndex = candleNumber - stepsBack * step;
-            _time = candles[newIndex].date.subtract(dif * stepsBack);
-          }
+          DateTime _time = _timeCalculator(step, index, dif);
           return Column(
             mainAxisSize: MainAxisSize.max,
             children: [
@@ -59,30 +100,6 @@ class TimeRow extends StatelessWidget {
             ],
           );
         },
-      ),
-    );
-  }
-
-  String numberFormat(int value) {
-    return "${value < 10 ? 0 : ""}$value";
-  }
-
-  Text _monthDayText(DateTime _time) {
-    return Text(
-      numberFormat(_time.day) + "/" + numberFormat(_time.month),
-      style: TextStyle(
-        color: ColorPalette.grayColor,
-        fontSize: 12,
-      ),
-    );
-  }
-
-  Text _hourMinuteText(DateTime _time) {
-    return Text(
-      numberFormat(_time.hour) + ":" + numberFormat(_time.minute),
-      style: TextStyle(
-        color: ColorPalette.grayColor,
-        fontSize: 12,
       ),
     );
   }
