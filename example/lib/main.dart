167f5c11e7aca9fd3bfbf6da99323eea7d485e6c
Reza Mirzaee <rmirzaee98@gmail.com>
2022-04-26 12:49:51 +0430

    feat: add moving average and bolinger bands indicator classes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:candlesticks/candlesticks.dart';
 import 'package:http/http.dart' as http;
-import 'dart:math' as math;
 
 void main() {
   runApp(MyApp());
@@ -17,51 +16,16 @@ class _MyAppState extends State<MyApp> {
   List<Candle> candles = [];
   bool themeIsDark = false;
   List<Indicator> indicators = [
-    Indicator(
-      name: "BB 20",
-      dependsOnNPrevCandles: 20,
-      calculator: (index, candles) {
-        double sum = 0;
-        for (int i = index; i < index + 20; i++) {
-          sum += candles[i].close;
-        }
-        final average = sum / 20;
-
-        num sumOfSquaredDiffFromMean = 0;
-        for (int i = index; i < index + 20; i++) {
-          final squareDiffFromMean = math.pow(candles[i].close - average, 2);
-          sumOfSquaredDiffFromMean += squareDiffFromMean;
-        }
-
-        final variance = sumOfSquaredDiffFromMean / 20;
-
-        final standardDeviation = math.sqrt(variance);
-
-        return [
-          average + standardDeviation * 2,
-          average,
-          average - standardDeviation * 2
-        ];
-      },
-      indicatorComponentsStyles: [
-        IndicatorStyle(name: "high", color: Colors.blue.shade900),
-        IndicatorStyle(name: "mid", color: Colors.yellow.shade800),
-        IndicatorStyle(name: "low", color: Colors.pink.shade800)
-      ],
+    BollingerBandsIndicator(
+      length: 20,
+      stdDev: 2,
+      upperColor: Color(0xFF2962FF),
+      basisColor: Color(0xFFFF6D00),
+      lowerColor: Color(0xFF2962FF),
     ),
-    Indicator(
-      name: "MA 100",
-      dependsOnNPrevCandles: 100,
-      calculator: (index, candles) {
-        double sum = 0;
-        for (int i = index; i < index + 100; i++) {
-          sum += candles[i].close;
-        }
-        return [sum / 100];
-      },
-      indicatorComponentsStyles: [
-        IndicatorStyle(name: "mv", color: Colors.green.shade600),
-      ],
+    WeightedMovingAverageIndicator(
+      length: 100,
+      color: Colors.green.shade600,
     ),
   ];
 
