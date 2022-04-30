0491bcfa9bc1e27172be97e8e981ac1ef4ae21a8
Reza Mirzaee <rmirzaee98@gmail.com>
2022-04-30 15:00:51 +0430

    fix: export candle_sticks_style from candlesticks.dart

--- lib/candlesticks.dart
+++ lib/candlesticks.dart
@@ -7,3 +7,4 @@ export 'src/models/indicator.dart';
 export 'src/utils/indicators/moving_average_indicator.dart';
 export 'src/utils/indicators/weighted_moving_average.dart';
 export 'src/utils/indicators/bollinger_bands_indicator.dart';
+export 'src/models/candle_sticks_style.dart';
