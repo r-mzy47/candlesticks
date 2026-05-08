123f23e98cab199be762bc946e8939a277558e42
rmirzaee <rmirzaee98@gmail.com>
2026-05-08 23:29:24 +0330

    chore: create gh-pages branch

--- lib/src/constant/view_constants.dart
+++
@@ -1,5 +0,0 @@
-const double PRICE_BAR_WIDTH = 60;
-const double DATE_BAR_HEIGHT = 20;
-const double MIN_PRICETILE_HEIGHT = 40;
-const double MAIN_CHART_VERTICAL_PADDING = 10;
-const double PRICE_INDICATOR_HEIGHT = 20;
